46b1ff9746e2ba1692a16a986f87102c1e82ebc6
Mauri Vinicius <viniciusmauri@outlook.com.br>
2023-02-22 15:58:34 -0300

    implementação model produto e fornecedor

--- src/main/java/br/com/mvsmtech/domain/dto/ProdutoDTO.java
+++ src/main/java/br/com/mvsmtech/domain/dto/ProdutoDTO.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.config.YamlProcessor;
 
 @Data
 @AllArgsConstructor
