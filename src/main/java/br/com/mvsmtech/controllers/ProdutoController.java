d11e3afdfae32e53db4e946acf8175b79e0bc4ea
Mauri Vinicius <viniciusmauri@outlook.com.br>
2023-04-18 02:33:03 -0300

    Start

--- src/main/java/br/com/mvsmtech/controllers/ProdutoController.java
+++ src/main/java/br/com/mvsmtech/controllers/ProdutoController.java
@@ -1,7 +1,15 @@
 package br.com.mvsmtech.controllers;
 
+import br.com.mvsmtech.domain.dto.ProdutoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProdutoController {
+
+    @GetMapping("/buscar")
+    private ResponseEntity<ProdutoDTO> {
+        
+    }
 }
---
+++ src/main/java/br/com/mvsmtech/repositories/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.mvsmtech.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository {
+}
